12e57a10f7f22106fd3abbcb6a38c349b857e459
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-04-22 15:53:35 -0500

    Remove team controller and repository

--- src/main/java/com/cs544/roommate/simple/TeamRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cs544.roommate.simple;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepo  extends CrudRepository<Team,Integer>{
-}
