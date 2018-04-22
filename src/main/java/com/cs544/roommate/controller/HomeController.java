2c057187b2b6f4ea86012e5b2ed7586bde55626a
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-04-22 17:21:50 -0500

    Home Controller

--- src/main/java/com/cs544/roommate/controller/HomeController.java
+++ src/main/java/com/cs544/roommate/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.cs544.roommate.controller;
 
 
+import java.util.LinkedList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,15 +15,14 @@ import com.cs544.roommate.domain.User;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/","/home","/index"})
+	
+    @RequestMapping(value = {"/","/home","/index"}, method = RequestMethod.GET)
     public ModelAndView home(){
-        ModelAndView mv = new ModelAndView("index");
-        return mv;
-    }
-    
-    @RequestMapping(value = {"/ratings"})
-    public ModelAndView ratings(){
-        ModelAndView mv = new ModelAndView("ratings");
+    	//getList() will give us list of properties as by default
+    	LinkedList<String> propertyList = getPropertyList();
+    	ModelAndView mv = new ModelAndView("index");
+        mv.addObject("propertyList", propertyList);
+        
         return mv;
     }
     
@@ -29,4 +31,16 @@ public class HomeController {
 		//model.addAttribute("person", new User());
 		return "signup";
 	}
+    
+    // Implementation to fetch list of properties 
+    private LinkedList<String> getPropertyList(){
+        LinkedList<String> properties = new LinkedList<String>();
+        
+        // Fetch the list of properties
+        properties.add("Item 1");
+        properties.add("Item 2");
+        properties.add("Item 3");
+
+        return properties;
+    }
 }
