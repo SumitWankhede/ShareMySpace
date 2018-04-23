084fa60d75876bd11dfd9fbf789db0ef96f87526
romiezaw <ymzaw@mum.edu>
2018-04-22 20:47:16 -0500

    Home page

--- src/main/java/com/cs544/roommate/controller/HomeController.java
+++ src/main/java/com/cs544/roommate/controller/HomeController.java
@@ -1,13 +1,10 @@
 package com.cs544.roommate.controller;
 
 
-import java.util.LinkedList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,14 +12,15 @@ import com.cs544.roommate.domain.User;
 
 @Controller
 public class HomeController {
-	
-    @RequestMapping(value = {"/","/home","/index"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/","/home","/index"})
     public ModelAndView home(){
-    	//getList() will give us list of properties as by default
-    	LinkedList<String> propertyList = getPropertyList();
-    	ModelAndView mv = new ModelAndView("index");
-        mv.addObject("propertyList", propertyList);
-        
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+    
+    @RequestMapping(value = {"/ratings"})
+    public ModelAndView ratings(){
+        ModelAndView mv = new ModelAndView("ratings");
         return mv;
     }
     
@@ -32,15 +30,9 @@ public class HomeController {
 		return "signup";
 	}
     
-    // Implementation to fetch list of properties 
-    private LinkedList<String> getPropertyList(){
-        LinkedList<String> properties = new LinkedList<String>();
-        
-        // Fetch the list of properties
-        properties.add("Item 1");
-        properties.add("Item 2");
-        properties.add("Item 3");
-
-        return properties;
+    @RequestMapping(value = {"/rooms"})
+    public ModelAndView rooms(){
+        ModelAndView mv = new ModelAndView("rooms");
+        return mv;
     }
 }
