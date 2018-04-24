1f140bddb7d0b17219a5a5120f77cabc705111ae
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-04-24 16:39:57 -0500

    Minor changes

--- src/main/java/com/cs544/roommate/repository/IPropertyDao.java
+++ src/main/java/com/cs544/roommate/repository/IPropertyDao.java
@@ -1,23 +1,26 @@
 package com.cs544.roommate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.cs544.roommate.domain.Property;
+import com.cs544.roommate.domain.PropertyType;
 
 
 // Not completed!!!
 
 public interface IPropertyDao extends JpaRepository<Property, Integer> {
 
-//	@Query("select p from property p where property_id = :id")
-//	public Room getRoom(@Param("id") int id);
-//	
-//	public void updateRoom(Room room);
-//	
-//	public Collection<Room> getRoomList();
-//	
-//	public void addRoom(Room room);
-//	
-//	public void removeRoom(int roomId);
+//	@Query("select distinct p from property p "
+//		      + " where "
+//		      + " LOWER(p.area) like CONCAT('%', LOWER(:location), '%') "
+//		      + "or p.room_type = :typeOfRoom " 
+//		      + "or p.available_rooms = :noOfRooms "
+//		      + "or p.room_price >= :budgetMin "
+//		      + "or p.room_price <= :budgetMax "
+//		      )
+//		  List<Property> search(@Param("location") String location,
+//		      @Param("typeOfRoom") PropertyType typeOfRoom, @Param("noOfRooms") int noOfRooms,
+//		      @Param("budgetMin") int budgetMin, @Param("budgetMax") int budgetMax);
 	
 }
