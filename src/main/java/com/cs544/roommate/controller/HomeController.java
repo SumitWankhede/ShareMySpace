89178125f2e02ae341c2f57e91d4b0b5fd7b3981
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-04-24 10:47:24 -0500

    Search in Home controller

--- src/main/java/com/cs544/roommate/controller/HomeController.java
+++ src/main/java/com/cs544/roommate/controller/HomeController.java
@@ -1,39 +1,51 @@
 package com.cs544.roommate.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.cs544.roommate.domain.User;
+import com.cs544.roommate.service.PropertyService;
+import com.cs544.roommate.utils.SearchParam;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/","/home","/index"})
-    public ModelAndView home(){
-        ModelAndView mv = new ModelAndView("index");
-        return mv;
-    }
-    
-    @RequestMapping(value = {"/ratings"})
-    public ModelAndView ratings(){
-        ModelAndView mv = new ModelAndView("ratings");
-        return mv;
-    }
-    
-    @RequestMapping(value = {"/details"})
-    public ModelAndView details(){
-        ModelAndView mv = new ModelAndView("details");
-        return mv;
-    }
-    
-    @RequestMapping(value = {"/rooms"})
-    public ModelAndView rooms(){
-        ModelAndView mv = new ModelAndView("rooms");
-        return mv;
-    }
-  
+	@Autowired
+	private PropertyService propertyService;
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("index");
+		((Model) mv).addAttribute("propertyList", propertyService.getPropertyList());
+
+		return mv;
+	}
+
+	@RequestMapping(value = { "/ratings" })
+	public ModelAndView ratings() {
+		ModelAndView mv = new ModelAndView("ratings");
+		return mv;
+	}
+
+	@RequestMapping(value = { "/details" })
+	public ModelAndView details() {
+		ModelAndView mv = new ModelAndView("details");
+		return mv;
+	}
+
+	@RequestMapping(value = { "/rooms" })
+	public ModelAndView rooms() {
+		ModelAndView mv = new ModelAndView("rooms");
+		return mv;
+	}
+
+	@PostMapping({ "/search" })
+	public String search(Model model, @ModelAttribute("search") SearchParam searchParam) {
+		model.addAttribute("propertyList",
+				propertyService.search(searchParam.getLocation(), searchParam.getTypeOfRoom(),
+						searchParam.getNoOfRooms(), searchParam.getBudgetMin(), searchParam.getBudgetMax()));
+		return "index";
+	}
 }
