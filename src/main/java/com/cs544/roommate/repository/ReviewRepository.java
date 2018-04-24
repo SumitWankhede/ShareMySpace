19e47f34028177eb25c355c69c3eb60c26ef2b31
romiezaw <ymzaw@mum.edu>
2018-04-23 23:58:39 -0500

    repo

--- src/main/java/com/cs544/roommate/repository/ReviewRepository.java
+++ src/main/java/com/cs544/roommate/repository/ReviewRepository.java
@@ -1,22 +1,24 @@
 package com.cs544.roommate.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.cs544.roommate.domain.Property;
 import com.cs544.roommate.domain.Review;
 //import com.cs544.roommate.domain.Tenant;
 import com.cs544.roommate.domain.User;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.*;
-
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>{
 	List<Review> getReviewByProperty(Property property);
 	List<Review> getReviewByUser(User user);
-	//List<Review> getAllReview(User user);
 	
+	//List<Review> getAllReview(User user);
+	@Query("select review from Review review join fetch review.property property where property.id = :propertyId")
+	List<Review> getReviewByPropertyId(@Param("propertyId") int propertyId);
 	
 }
